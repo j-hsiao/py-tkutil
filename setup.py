6a053417a53667689ec8674f17d4a4c055bdb4b3
jason hsiao <oaishnosaj@gmail.com>
2022-09-05 17:38:10 +0100

    update setup.py for newest setuptools
    newest setuptools fails namespace package unless it is explicitly added

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     author='Jason Hsiao',
     author_email='oaishnosaj@gmail.com',
     description='tkinter helpers',
+    packages=['jhsiao'],
     py_modules=['jhsiao.tkutil.util'],
     install_requires=[
         'jhsiao-utils @ git+https://github.com/j-hsiao/py-utils.git',

e824a8347923bafa7c7b6e4e516f615f1e5c6a47
Jason Hsiao <oaishnosaj@gmail.com>
2023-06-10 09:51:07 +0100

    Update tests to use updated interface

--- tests/bindings.py
+++ tests/bindings.py
@@ -40,11 +40,21 @@ def test_wrapper():
     def func(widget, x, y):
         succeeded.extend(
             ('widget', widget, 'event successfully fired at', x, y))
+        widget.after_cancel(warnid)
         widget.destroy()
     w = bindings.Wrapper(func)
     w.bind(r)
+    l = tk.Label(
+        r,
+        text=(
+            'Please give this window focus if it does not have it.'
+            ' Otherwise, press q to end the test.'))
+    l.grid(row=0, column=0)
+    r.grid_rowconfigure(0, minsize=300)
+    r.grid_columnconfigure(0, minsize=300)
     r.bind('<q>', str(w))
-    r.after(1000, r.event_generate, '<q>')
+    r.after(3000, r.event_generate, '<q>')
+    warnid = r.after(5000, print, 'press q to end test, focus probably was not given in time')
     r.mainloop()
     assert succeeded
     print(*succeeded)
@@ -60,14 +70,22 @@ def test_bindings():
             allbinds.apply(self)
             self.success = False
             self.clicks = 0
+            self.grid_rowconfigure(0, minsize=300, weight=1)
+            self.grid_columnconfigure(0, minsize=300, weight=1)
             f = tk.Frame(self)
-            f.grid()
-            l = tk.Label(f, text='click on me!')
-            l.grid()
+            f.grid(row=0, column=0, sticky='nsew')
+            f.grid_rowconfigure(0, weight=1)
+            f.grid_columnconfigure(0, weight=1)
+            l = tk.Label(
+                f,
+                text='click on me!, press q to stop test if not focused in 3 secs')
+            l.grid(sticky='nsew')
+
 
         @b.bind('<q>')
         def stop(widget):
             widget.success = True
+            widget.after_cancel(warnid)
             widget.destroy()
 
         @staticmethod
@@ -80,6 +98,7 @@ def test_bindings():
     # It seems event_generate does not do anything
     # if the tk app does not have focus.
     d.after(3000, d.event_generate, '<q>')
+    warnid = d.after(5000, print, 'Focus was not given, press q to exit test')
     d.after(50000, d.stop)
     d.mainloop()
     assert(d.success)
@@ -88,23 +107,28 @@ def test_bindings():
 def test_script():
     binds = bindings.Bindings()
 
-    @binds('')
+    @binds()
     def func(widget):
         print('got a widget:', widget)
 
     r = tk.Tk()
+    r.grid_rowconfigure(0, minsize=300, weight=1)
+    r.grid_columnconfigure(0, minsize=300, weight=1)
     b = tk.Button(
         r, text='click me'
     )
     b.configure(command=str(func.update(widget=(str(b), None))))
-    b.grid()
+    b.grid(row=0, column=0, sticky='nsew')
 
-    @binds[str(r)].bind('<Escape>')
+    @binds(str(r), '<Escape>')
     def end(widget):
         print('Escape done')
+        widget.after_cancel(warnid)
         widget.winfo_toplevel().destroy()
 
     binds.apply(r)
+    r.after(5000, r.event_generate, '<Escape>')
+    warnid = r.after(6000, lambda : b.configure(text='unfocused, press escape to exit'))
 
     r.mainloop()
 
